4f283ffebbade59268d54ef05a552be2f761f16f
Kate <hamster.k.po@gmail.com>
2026-02-27 19:15:32 +0200

    dog's age boundary at 29 coverage

--- app/test_main.py
+++ app/test_main.py
@@ -12,12 +12,15 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (28, 29, [3, 3]),
         (100, 100, [21, 17]),
     ],
 )
-def test_get_human_age_valid_cases(cat_age: int,
-                                   dog_age: int,
-                                   expected: list) -> None:
+def test_get_human_age_valid_cases(
+    cat_age: int,
+    dog_age: int,
+    expected: list[int],
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
